8505d476cc5899a1a0c9371aecac7bf9b91b10d5
er-shubhendu-ee <38459865+er-shubhendu-ee@users.noreply.github.com>
2023-09-11 21:57:50 +0530

    circular-queue-conflict resolved

---
+++ ddl_log.h
@@ -0,0 +1,3 @@
+#include "ddl_base.h"
+
+#define DDL_LOG(x) printf("%s : %d : %s\n", __func__, __LINE__, x)
--- main.c
+++ main.c
@@ -1,63 +1,222 @@
 /**
  * @file      main.c
  * @author:   Shubhendu B B
- * @date:     18/05/2023
+ * @date:     04/02/2023
  * @brief
  * @details
  *
  * @copyright
  *
  **/
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "app_queue.h"
+#include "ddl_queue.h"
+
+typedef struct {
+  uint8_t byte_1;
+  uint16_t word_1;
+  uint32_t dword_1;
+} QueueElement_t;
+
+#define QUEUE_ELEMENT_COUNT 5
+QueueElement_t queueElementArray[QUEUE_ELEMENT_COUNT] = {0};
+
+ddl_queue_handle_t queue, queue_2;
 
 int main(void) {
-  printf("%s : %d : started!\r\n", __func__, __LINE__);
+  QueueElement_t queueElement = {0};
+  queue = ddl_queue_create_static(sizeof(QueueElement_t), QUEUE_ELEMENT_COUNT,
+                                  (uint8_t *)&queueElementArray[0]);
 
-  logStorageStruct_t logStorageStructArrayTemp[LOG_STORAGE_COUNT_MAX] = {0};
-  storageQueueHandle_t logStorageQueueHandle = NULL;
-  logStorageQueueHandle = app_helper_StorageQueueInit();
-
-  if (logStorageQueueHandle == NULL) {
-    printf("%s : %d : Error creating queue.\r\n", __func__, __LINE__);
-    return -1;
+  queue_2 = ddl_queue_create_static(sizeof(QueueElement_t), QUEUE_ELEMENT_COUNT,
+                                    (uint8_t *)&queueElementArray[0]);
+  queueElement.byte_1 = 1;
+  queueElement.word_1 = 11;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
   }
 
-  app_helper_QueuePut(1, 10, 1);
-  app_helper_QueuePut(2, 10, 2);
-  app_helper_QueuePut(3, 10, 3);
-  app_helper_QueuePut(4, 10, 4);
-  app_helper_QueuePut(5, 10, 5);
-  app_helper_QueuePut(6, 5, 6);
+  queueElement.byte_1 = 2;
+  queueElement.word_1 = 22;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_QueueGet(logStorageStructArrayTemp, 1);
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
 
-  app_helper_QueuePut(6, 10, 6);
+  queueElement.byte_1 = 3;
+  queueElement.word_1 = 33;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_QueueGet(logStorageStructArrayTemp, 1);
+  queueElement.byte_1 = 4;
+  queueElement.word_1 = 44;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_QueuePut(7, 10, 7);
+  queueElement.byte_1 = 5;
+  queueElement.word_1 = 55;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_QueueGet(logStorageStructArrayTemp, 1);
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
 
-  app_helper_QueuePut(8, 10, 8);
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
 
-  app_helper_QueueGet(logStorageStructArrayTemp, 1);
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
 
-  app_helper_QueuePut(9, 10, 9);
-  app_helper_QueuePut(10, 10, 10);
+  queueElement.byte_1 = 6;
+  queueElement.word_1 = 66;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_PrintStorageQueue();
+  queueElement.byte_1 = 7;
+  queueElement.word_1 = 77;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  printf("%s : %d : %d logs read success.\r\n", __func__, __LINE__,
-         app_helper_QueueGet(logStorageStructArrayTemp, 1));
+  queueElement.byte_1 = 8;
+  queueElement.word_1 = 88;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_PrintStorageQueue();
+  queueElement.byte_1 = 9;
+  queueElement.word_1 = 99;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  printf("%s : %d : %d logs read success.\r\n", __func__, __LINE__,
-         app_helper_QueueGet(logStorageStructArrayTemp, 3));
+  queueElement.byte_1 = 10;
+  queueElement.word_1 = 1010;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
 
-  app_helper_PrintStorageQueue();
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
+
+  queueElement.byte_1 = 11;
+  queueElement.word_1 = 1111;
+  queueElement.dword_1 = 123456789;
+  if (ddl_queue_send(queue, &queueElement)) {
+    printf("%s : %d : Error: Failed to send to queue.\r\n", __func__, __LINE__);
+  }
+
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
+
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
+
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
+
+  memset(&queueElement, 0, sizeof(queueElement));
+  if (ddl_queue_recv(queue, &queueElement)) {
+    printf("%s : %d : : Error: Failed to receive from queue.", __func__,
+           __LINE__);
+  }
+  printf(
+      "%s : %d : received value queueElement.byte_1: %d, queueElement.word_1: "
+      "%d "
+      "queueElement.dword_1: %d\r\n",
+      __func__, __LINE__, queueElement.byte_1, queueElement.word_1,
+      queueElement.dword_1);
 
   memset(&queueElement, 0, sizeof(queueElement));
   if (ddl_queue_recv(queue, &queueElement)) {
